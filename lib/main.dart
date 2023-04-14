b684dc43e677646652a6c8887a853d91c51ad029
upendraTRL <upendrataral21@gmail.com>
2023-04-14 23:31:41 +0530

    Lat n long, inserted.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:testflutter/mongodb.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await MongoDatabase.connect();
+  Future.delayed(Duration(seconds: 5), () {
+    MongoDatabase.connect();
+  });
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   // This widget is the root of your application.
+
   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Loc",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: "Home Page"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String locationMessage = "Current location";
+  late String lat;
+  late String long;
+
+  @override
+  void initState() {
+    print("object");
+    super.initState();
+    _getCurrentLocation().then((value) {
+      lat = '${value.latitude}';
+      long = '${value.longitude}';
+      getLatLong(lat, long);
+    });
+  }
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error("Locatoon permission are denied");
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error("Location permissions are permanently denied");
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -53,100 +124,25 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              print("Clicked");
-            },
-            child: Text("S.O.S"),
-            style: ElevatedButton.styleFrom(primary: Colors.red),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            //Location display
+            Text(locationMessage, textAlign: TextAlign.center),
+            const SizedBox(height: 20),
+            const SizedBox(height: 20),
+
+            //SOS button
+            ElevatedButton(
+              onPressed: () {
+                print("Geo loc");
+              },
+              child: Text("S.O.S"),
+              style: ElevatedButton.styleFrom(primary: Colors.red),
             ),
           ],
-        ),
+        )),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/mongodb.dart
+++ lib/mongodb.dart
@@ -2,26 +2,30 @@ import 'dart:developer';
 
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:testflutter/constant.dart';
+import 'package:testflutter/location.dart';
+
+var lat;
+var long;
+
+void getLatLong(gLat, gLong) {
+  lat = gLat;
+  long = gLong;
+  print("Latiiiii");
+}
 
 class MongoDatabase {
   static connect() async {
-    var db = await Db.create(MONGO_URL);  //Connection created
+    var db = await Db.create(MONGO_URL); //Connection created
     await db.open();
     inspect(db);
     var status = db.serverStatus();
     print(status);
-    var collection = db.collection(COLLECTION_NAME);  //Collection specified
-    await collection.insertMany([
-      {
-        "username": "Ghosty",
-        "name": "Chinmay",
-      },
-      {
-        "username": "FatesEnd",
-        "name": "Upendra",
-      },
-    ]);
-    print("Hello");
-    print(await collection.find().toList());
+    print(lat);
+    var collection = db.collection(COLLECTION_NAME); //Collection specified
+    // print(await collection.find().toList());
+    await collection.insertOne({
+      "latitude": lat,
+      "longitude": long,
+    });
   }
 }
